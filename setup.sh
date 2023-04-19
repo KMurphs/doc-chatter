b67e1ab325048649643eee9a92840198a645bf2f
Stephane K <kibonges@amazon.com>
2023-04-19 19:20:41 +0200

    Refactor

--- setup.sh
+++ setup.sh
@@ -3,10 +3,10 @@
 
 mv ~/.bb_worspace.sh ~/.bb_worspace-backup.sh || echo "File .bb_worspace.sh not found"
 wdir="$(dirname "$0")"
-cp $wdir/src/bb_worspace.sh ~/.bb_worspace.sh
+cp $wdir/bash/bb_worspace.sh ~/.bb_worspace.sh
 chmod +x ~/.bb_worspace.sh
 
 mv ~/.zshrc ~/.zshrc-backup || echo "File .zshrc not found"
 wdir="$(dirname "$0")"
-cp $wdir/src/zshrc ~/.zshrc
+cp $wdir/bash/zshrc ~/.zshrc
 . ~/.zshrc
