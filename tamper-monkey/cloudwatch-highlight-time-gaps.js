0edc2bbf8f6801e2a98a17a6c9432db804c621a5
Stephane Kibonge <kibonges@amazon.com>
2023-10-03 10:22:39 +0000

    Refactor script

--- tamper-monkey/cloudwatch-highlight-time-gaps.js
+++ tamper-monkey/cloudwatch-highlight-time-gaps.js
@@ -13,18 +13,36 @@ const LOG_ROW_SELECTOR = ".logs-table__body-row";
 const LOG_TIMESTAMP_CELL_SELECTOR = ".logs-table__body-cell:nth-child(2)";
 const SECONDS_GAP_TO_FIND = 60;
 const HIGHLIGHT_COLOR = "#db7f7f";
+const SCRIPT_NAME = "CloudWatch Time Gaps";
+const CLOUDWATCH_SCHEDULER = "cloudwatchScriptScheduler";
+const SUBDOCUMENT_CONTAINER_SELECTOR = "#microConsole-Logs";
 
 (function() {
     'use strict';
-    console.log("CloudWatch Time Gaps: Loaded");
-
     // Only run on log-insights
     if (!(/logs-insights/.test(location.href))) return;
 
-    const rows = document.querySelectorAll(LOG_ROW_SELECTOR);
-    Array
-        .from(rows)
-        .map(elt => elt.querySelectorAll(LOG_TIMESTAMP_CELL_SELECTOR))
-        .filter((elt, i) => i > 0 && (Math.abs(new Date(elt.innerText) - new Date(rows[i - 1].innerText)) > (SECONDS_GAP_TO_FIND * 1000)))
-        .map(elt => elt.style.backgroundColor = HIGHLIGHT_COLOR)
+    // Setup scripts to run every x secs
+    if(!window[CLOUDWATCH_SCHEDULER]){
+         console.log("Setting up Cloudwatch Script Scheduler");
+         // Map from script names to script functions
+         window[CLOUDWATCH_SCHEDULER] = {};
+        // Side effect runner
+        setInterval(() => {
+            Object.values(window[CLOUDWATCH_SCHEDULER]).map(fn => fn && fn());
+        }, 1000);
+    }
+
+    // Setup script function
+    console.log(`${SCRIPT_NAME}: Loaded`);
+    window[CLOUDWATCH_SCHEDULER][SCRIPT_NAME] = () => {
+        const subDocument = document.querySelector(SUBDOCUMENT_CONTAINER_SELECTOR).contentWindow.document;
+        const rows = subDocument.querySelectorAll(LOG_ROW_SELECTOR);
+
+        Array
+            .from(rows)
+            .map(elt => elt.querySelector(LOG_TIMESTAMP_CELL_SELECTOR))
+            .filter((elt, i, rows) => i > 0 && (Math.abs(new Date(elt.innerText) - new Date(rows[i - 1].innerText)) > (SECONDS_GAP_TO_FIND * 1000)))
+            .map(elt => (elt.style.backgroundColor = HIGHLIGHT_COLOR));
+    }
 })();
